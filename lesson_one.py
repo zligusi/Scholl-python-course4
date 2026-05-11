f7c0fb598828985aa30ccb3556de7b47a972b092
zligusi <tort24abcd@gmaill.com>
2026-05-11 17:03:32 +0200

    file lesson no commit

--- lesson_one.py
+++ lesson_one.py
@@ -1,3 +1,20 @@
 #new course for python3 
 # GO Go GO 
 
+# print this is bazza ... 
+
+# print(this is \" bed \" pepole")
+#RESULTAT :
+# this is "bed" pepole
+
+#name = "Kyrylo"
+#metod 1 
+#print(f("Hello {name}"))
+
+#metod 2
+# print("Hello " + name)
+
+#RESULTAT :  
+# Hello Kyrylo
+
+# input  bazza ... 

00e0692484018c0503cb31e5f91faab88f5322fb
zligusi <tort24abcd@gmaill.com>
2026-05-11 20:30:37 +0200

    kalkulator fatal

--- kalkylator.py
+++ kalkylator.py
@@ -1,2 +1,23 @@
 # The cod is a mini kalkylator 
 
+want = input("Что делаем ? (+ , - ,  * ,  : )")
+
+while True :
+    try :
+        a = int(input("Введите первое число : "))
+        b = int(input("Введите второе число : "))
+        
+        if want == "+" :
+            c = a + b 
+        elif want == "-" :
+            c = a - b 
+        elif want == "*" :
+            c = a * b
+        elif want == ":" :
+            c = a / b
+        else :
+            print("Вы ввели не правильный знак операции ")
+        break
+    except ValueError :
+        print("Вы ввели не число ")
+print(f"Результат : {c}")
